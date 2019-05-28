32815fc550c7aa9a0d784c5c3898c76637561599
HOPMAJIbHbIN <36517889+HOPMAJIbHbIN@users.noreply.github.com>
2019-05-28 13:27:44 +0300

    Add files via upload

---
+++ Not django labs/Lr2.py
@@ -0,0 +1,36 @@
+﻿print ("Задание 1")
+import random
+my_number=random.randint(0,10)
+user_number=int(input("Введите число: "))
+if user_number==my_number:
+    print("Введено число равное заданному.")
+    print("Заданное число:",my_number)
+else:
+    while True:
+        user_number=int(input("Ваше число не равно заданному, введите снова: "))
+        if(user_number==my_number):
+            print("Введено число равно заданному.")
+            print("Заданное число:",my_number)
+            break
+
+print ("Задание 2")
+l = ['2312132amvr','qwert','21d32rf3r24g34','1548964865668887','123','688','0']
+print("Строки размером до 10 символов")
+for item in l:
+    if((len(item)<=10)):
+        print(item)
+
+print ("Задание 3")
+import string
+
+N=int(input("Введите число: "))
+r="R"
+print (r*N)
+
+print ("Задание 4")
+import re
+s = input('Введите строку:')
+s = re.sub(r"\d+", "", s, flags=re.UNICODE)
+print (s)
+
+input('Press Enter to exit')
